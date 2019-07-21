0ce97433c97f679d59b96e7f676157c5e9af8d86
Mahmoud Bwedany <mahmoudbwedany@gmail.com>
2019-07-21 22:51:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+class BoardMember {
+  constrocutor (name, homeState, training){
+    this.training = training;
+    this.name = name;
+    this.homeState = homeState;
+    
+    
+  }
+  
+  veto (){
+    return ("No, I must disagree");
+    
+  }
+  
+}
