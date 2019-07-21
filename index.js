5b7f6c8079582daa15981de7ab7da4b374d6e7d4
Mahmoud Bwedany <mahmoudbwedany@gmail.com>
2019-07-21 23:08:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,4 +25,8 @@ class BoardMember {
     
   }
   
+  sayHi () {
+    return ("Hi, my name is " + this.name + "." + "I am from" + this.homeState + ", and I was trained in " + this.training);
+  }
+  
 }
